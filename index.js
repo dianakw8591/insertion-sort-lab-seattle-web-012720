f5f4e270f9383c25852d066dfb735d75ae2b1202
dianakw8591 <dianakw@gmail.com>
2020-07-15 12:27:48 -0700

    Done

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  let min = array[0];
+  let index = 0;
+  array.forEach((elem, i) => {
+    if (elem < min) {
+      min = elem;
+      index = i;
+    }
+  })
+  array.splice(index, 1);
+  return min
 }
 
 function insertionSort(array){
-
+  let sorted = [];
+  while (array.length > 0) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
